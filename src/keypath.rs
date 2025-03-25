7889953244358d7312e8a4e66707e7b8a790aa6d
Dan Fego <dan.fego@gmail.com>
2025-03-25 09:44:28 -0400

    Moving towards keypath completion

--- src/keypath.rs
+++ src/keypath.rs
@@ -1,30 +1,54 @@
-//! Parsing for keypaths, which are of the form:
+//! Parsing for keypaths, which are flexible keys for navigating a datastore.
+//! Keypath components may represent either path components or keys within a YAML file.
 //!
-//! a.b.c.d
+//! # Format
+//! Keypaths are of the form `a.b.c.d`, where `a`, `b`, `c`, and `d` are keys, and `.` are delimiters.
+//! Forward-slash characters (i.e. `/`) must not be contained in a path, and no components may be empty.
+//! This means in effect that the [delimiter](DELIMITER) (`.`) may not appear twice in a row or appear at the beginning or end of a keypath.
 //!
-//! The only things precluded from this design are file components or
-//! YAML keys with dots in them, and that the key may not have empty components,
-//! i.e. two dots in a row.
+//! ## Spaces
+//! If a keypath contains spaces at the beginning or end of a component, those spaces will be stripped.
+//! For example, ` a . b . c . d ` will be parsed as `a.b.c.d.`.
 //!
-//! Might make sense to disallow slashes, too?
+//! ## Invalid Examples
+//! The following are some examples of invalid keypaths:
 //!
-//! For each component, the following are tried, in this order, until one is true:
+//! * `contains/slash`
+//! * `empty.component..in.middle`
+//! * `whitespace.component. .in.middle`
+//! * `.empty.component.at.beginning`
+//! * `empty.component.at.end.`
 //!
-//! TODO move all this to the main file where it's relevant.
+//! # Usage
+//! Keypaths provide [iterators](`KeyPath::iter`) that can be used to iterate over all possible interpretations of a keypath.
 //!
-//! 1. Is there a directory with this name at the current level from the datastore root?
-//! 2. Is there file with this name and a .yaml extension at this level?
-//! 3. Is there a file with this name and a .yml extension at this level?
-//! 4. If we've matched a file (2 or 3 above), is there a key at the current level?
+//! ```rust
+//! use yaml_datastore::keypath::KeyPath;
 //!
-//! If at any point these all fail, data parsing will fail.
-use core::num;
-use std::{
-    fmt::Display,
-    iter::{Zip, zip},
-    path::{self, Path, PathBuf},
-};
+//! let keypath = KeyPath::try_from("a.b.c").expect("keypath parsed");
+//! for (path, keys) in keypath.iter() {
+//!     println!("{:10} | {:?}", path.display(), keys);
+//! }
+//! ```
+//!
+//! The above should print out:
+//!
+//! ```text
+//! a/b/c.yaml | []
+//! a/b/c.yml  | []
+//! a/b.yaml   | ["c"]
+//! a/b.yml    | ["c"]
+//! a.yaml     | ["b", "c"]
+//! a.yml      | ["b", "c"]
+//! ```
+//!
+//! This iterator can then be used to search the keystore with the given precedence: directories > files > keys.
+
+use std::{ffi::OsStr, path::PathBuf};
 use thiserror::Error;
+
+/// Default file extensions for the iterator.
+pub static DEFAULT_EXTENSIONS: &[&str] = &["yaml", "yml"];
 
 /// Delimiter on which components of a keypath are split.
 const DELIMITER: &str = ".";
@@ -44,15 +68,16 @@ pub enum KeyPathParseError {
 
 /// Internal struct for parsing and managing keypath components.
 ///
-/// The only way to construct is [`try_from`].
+/// Construct using [`try_from`](KeyPath::try_from).
 #[derive(Debug)]
-pub(crate) struct KeyPath {
+pub struct KeyPath {
     /// Raw string that components point to.
     raw: String,
 }
 
 /// Check a single keypath component for validity and return a String if it's valid.
 fn validate_and_trim(component: &str) -> Result<&str, KeyPathParseError> {
+    let component = component.trim();
     if component.is_empty() || component.contains(INVALID_CHARACTERS) {
         Err(KeyPathParseError::InvalidKeyPath)
     } else {
@@ -63,6 +88,12 @@ fn validate_and_trim(component: &str) -> Result<&str, KeyPathParseError> {
 impl TryFrom<&str> for KeyPath {
     type Error = KeyPathParseError;
 
+    /// Construct a [`KeyPath`] from a string.
+    ///
+    /// A valid `KeyPath` is a string with some components separated by `.`.
+    /// See the [module-level documentation](crate::keypath) for details.
+    ///
+    /// # Errors
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         // Split up value, validate and trim it, then put it back together.
         Ok(Self {
@@ -75,102 +106,64 @@ impl TryFrom<&str> for KeyPath {
     }
 }
 
-impl Display for KeyPath {
+impl std::fmt::Display for KeyPath {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.raw)
     }
 }
 
-const EXTENSIONS: &[&str] = &["yaml", "yml"];
-
 impl KeyPath {
-    pub fn components(&self) -> Vec<&str> {
-        self.raw.split(DELIMITER).collect()
+    /// Return an iterator over the keypath components using the [default list of extensions][DEFAULT_EXTENSIONS].
+    ///
+    ///
+    pub fn iter(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
+        self.iter_extensions(DEFAULT_EXTENSIONS)
     }
 
-    // pub fn split_iter(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
-    //     let c = self.components();
-    //     let c2 = c.clone();
-    //     let range = (1..=c.len()).rev();
-    //     zip(
-    //         range.clone().flat_map(move |i| {
-    //             let path: PathBuf = c[0..i].iter().collect();
-    //             [path.with_extension("yaml"), path.with_extension("yml")]
-    //         }),
-    //         range
-    //             .clone()
-    //             .flat_map(move |i| [c2[i..].to_vec(), c2[i..].to_vec()]),
-    //     )
-    // }
-
-    // pub fn split_iter2(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
-    //     let c = self.components();
-    //     let c2 = c.clone();
-    //     let range = (1..=c.len()).rev();
-    //     zip(
-    //         range.clone().map(move |i| c[0..i].iter().collect()),
-    //         range.clone().map(move |i| c2[i..].to_vec()),
-    //     )
-    //     .flat_map(|pair: (PathBuf, _)| {
-    //         [
-    //             (pair.0.with_extension("yaml"), pair.1.clone()),
-    //             (pair.0.with_extension("yml"), pair.1),
-    //         ]
-    //     })
-    // }
-
     /// Return an iterator
-    pub fn iter(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
+    pub fn iter_extensions<S: AsRef<OsStr>>(
+        &self,
+        extensions: &[S],
+    ) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
         let paths = self.components();
         let keys = self.components();
         let range = (1..=paths.len()).rev();
-        zip(
+        std::iter::zip(
             range.clone().map(move |i| paths[0..i].iter().collect()),
             range.clone().map(move |i| keys[i..].to_vec()),
         )
         .flat_map(|pair: (PathBuf, _)| {
-            EXTENSIONS
+            extensions
                 .iter()
                 .map(move |e| (pair.0.with_extension(e), pair.1.clone()))
         })
     }
 
-    // pub fn split_iter2(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
-    //     let c1 = self.components();
-    //     let c2 = self.components();
-    //     let range = (1..=c1.len()).rev();
-    //     let path_iterator = range.clone().map(move |i| c1[0..i].iter().collect());
-    //     let key_iterator = range.clone().map(move |i| c2[i..].to_vec());
-    //     fn add_extensions(v: (PathBuf, Vec<&str>)) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
-    //         EXTENSIONS
-    //             .iter()
-    //             .map(move |e| (v.0.with_extension(e), v.1.clone()))
-    //     }
-    //     zip(path_iterator, key_iterator).flat_map(add_extensions)
-    // }
-
-    // pub fn split_iter4(&self) -> impl Iterator<Item = (PathBuf, Vec<&str>)> {
-    //     let components = self.components();
-    //     let mut ret = vec![];
-    //     for index in (1..=components.len()).rev() {
-    //         let path: PathBuf = components[0..index].iter().collect();
-    //         let key_vec = components[index..].to_vec();
-    //         for extension in EXTENSIONS {
-    //             ret.push((path.with_extension(extension), key_vec.clone()));
-    //         }
-    //     }
-    //     ret.into_iter()
-    // }
+    /// Return the parsed components as a list of strings.
+    ///
+    /// While not intended for use externally at this point, it could be useful for introspection.
+    pub fn components(&self) -> Vec<&str> {
+        self.raw.split(DELIMITER).collect()
+    }
 }
 
 #[cfg(test)]
-mod adhoc_tests {
+mod tests {
     use super::*;
 
     #[test]
-    fn adhoc() {
+    fn valid() {
+        let input = "this.is.a.valid.keypath";
+        let result = KeyPath::try_from(input).expect("key parsed");
+        let expected = vec!["this", "is", "a", "valid", "keypath"];
+        assert_eq!(result.components(), expected);
+        assert_eq!(result.to_string(), input);
+    }
+
+    #[test]
+    fn iterator() {
         let input = "this.is.a.keypath";
-        let result = KeyPath::try_from(input).unwrap();
+        let result = KeyPath::try_from(input).expect("key parsed");
         let zipped: Vec<_> = result.iter().collect();
         let expected = vec![
             (PathBuf::from("this/is/a/keypath.yaml"), vec![]),
@@ -183,109 +176,31 @@ mod adhoc_tests {
             (PathBuf::from("this.yml"), vec!["is", "a", "keypath"]),
         ];
         assert_eq!(zipped, expected);
-
-        // for (a, b) in zipped {
-        //     println!("{}, {:?}", a.display(), b);
-        // }
     }
 
-    // #[test]
-    // fn adhoc2() {
-    //     let input = "this.is.a.keypath";
-    //     let result = KeyPath::try_from(input).unwrap();
-    //     let zipped: Vec<_> = result.split_iter2().collect();
-    //     let expected = vec![
-    //         (PathBuf::from("this/is/a/keypath.yaml"), vec![]),
-    //         (PathBuf::from("this/is/a/keypath.yml"), vec![]),
-    //         (PathBuf::from("this/is/a.yaml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is/a.yml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is.yaml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this/is.yml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this.yaml"), vec!["is", "a", "keypath"]),
-    //         (PathBuf::from("this.yml"), vec!["is", "a", "keypath"]),
-    //     ];
-    //     assert_eq!(zipped, expected);
-
-    //     // for (a, b) in zipped {
-    //     //     println!("{}, {:?}", a.display(), b);
-    //     // }
-    // }
-
-    // #[test]
-    // fn adhoc3() {
-    //     let input = "this.is.a.keypath";
-    //     let result = KeyPath::try_from(input).unwrap();
-    //     let zipped: Vec<_> = result.iter().collect();
-    //     let expected = vec![
-    //         (PathBuf::from("this/is/a/keypath.yaml"), vec![]),
-    //         (PathBuf::from("this/is/a/keypath.yml"), vec![]),
-    //         (PathBuf::from("this/is/a.yaml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is/a.yml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is.yaml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this/is.yml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this.yaml"), vec!["is", "a", "keypath"]),
-    //         (PathBuf::from("this.yml"), vec!["is", "a", "keypath"]),
-    //     ];
-    //     assert_eq!(zipped, expected);
-
-    //     // for (a, b) in zipped {
-    //     //     println!("{}, {:?}", a.display(), b);
-    //     // }
-    // }
-
-    // #[test]
-    // fn adhoc4() {
-    //     let input = "this.is.a.keypath";
-    //     let result = KeyPath::try_from(input).unwrap();
-    //     let zipped: Vec<_> = result.split_iter4().collect();
-    //     let expected = vec![
-    //         (PathBuf::from("this/is/a/keypath.yaml"), vec![]),
-    //         (PathBuf::from("this/is/a/keypath.yml"), vec![]),
-    //         (PathBuf::from("this/is/a.yaml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is/a.yml"), vec!["keypath"]),
-    //         (PathBuf::from("this/is.yaml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this/is.yml"), vec!["a", "keypath"]),
-    //         (PathBuf::from("this.yaml"), vec!["is", "a", "keypath"]),
-    //         (PathBuf::from("this.yml"), vec!["is", "a", "keypath"]),
-    //     ];
-    //     assert_eq!(zipped, expected);
-
-    //     // for (a, b) in zipped {
-    //     //     println!("{}, {:?}", a.display(), b);
-    //     // }
-    // }
-}
-
-// struct Iterator<'a> {
-//     keypath: &'a KeyPath,
-//     index: u32,
-// }
-
-// impl<'a> Iterator for KeyPathIterator<'a> {
-//     type Item = (std::path::PathBuf, Vec<String>);
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if index > 0
-//     }
-// }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
     #[test]
-    fn valid() {
-        let input = "this.is.a.valid.keypath";
-        let result = KeyPath::try_from(input).unwrap();
-        let expected = vec!["this", "is", "a", "valid", "keypath"];
-        assert_eq!(result.components(), expected);
-        assert_eq!(result.to_string(), input);
+    fn iterator_with_explicit_extensions() {
+        let input = "this.is.a.keypath";
+        let result = KeyPath::try_from(input).expect("key parsed");
+        let extensions = vec!["json", "xml"];
+        let zipped: Vec<_> = result.iter_extensions(&extensions).collect();
+        let expected = vec![
+            (PathBuf::from("this/is/a/keypath.json"), vec![]),
+            (PathBuf::from("this/is/a/keypath.xml"), vec![]),
+            (PathBuf::from("this/is/a.json"), vec!["keypath"]),
+            (PathBuf::from("this/is/a.xml"), vec!["keypath"]),
+            (PathBuf::from("this/is.json"), vec!["a", "keypath"]),
+            (PathBuf::from("this/is.xml"), vec!["a", "keypath"]),
+            (PathBuf::from("this.json"), vec!["is", "a", "keypath"]),
+            (PathBuf::from("this.xml"), vec!["is", "a", "keypath"]),
+        ];
+        assert_eq!(zipped, expected);
     }
 
     #[test]
     fn valid_with_spaces() {
         let input = " this . is . a . valid . keypath ";
-        let result = KeyPath::try_from(input).unwrap();
+        let result = KeyPath::try_from(input).expect("key parsed");
         let expected = vec!["this", "is", "a", "valid", "keypath"];
         assert_eq!(result.components(), expected);
         assert_eq!(result.to_string(), "this.is.a.valid.keypath");
@@ -294,28 +209,35 @@ mod tests {
     #[test]
     fn err_contains_slash() {
         let input = "contains/slash";
-        let result = KeyPath::try_from(input).unwrap_err();
+        let result = KeyPath::try_from(input).expect_err("invalid keypath");
         assert!(matches!(result, KeyPathParseError::InvalidKeyPath));
     }
 
     #[test]
     fn err_empty_component_middle() {
-        let input = "has..component";
-        let result = KeyPath::try_from(input).unwrap_err();
+        let input = "empty.component..in.middle";
+        let result = KeyPath::try_from(input).expect_err("invalid keypath");
+        assert!(matches!(result, KeyPathParseError::InvalidKeyPath));
+    }
+
+    #[test]
+    fn err_whitespace_component_middle() {
+        let input = "whitespace.component. .in.middle";
+        let result = KeyPath::try_from(input).expect_err("invalid keypath");
         assert!(matches!(result, KeyPathParseError::InvalidKeyPath));
     }
 
     #[test]
     fn err_empty_component_first() {
-        let input = ".has.component";
-        let result = KeyPath::try_from(input).unwrap_err();
+        let input = ".empty.component.at.beginning";
+        let result = KeyPath::try_from(input).expect_err("invalid keypath");
         assert!(matches!(result, KeyPathParseError::InvalidKeyPath));
     }
 
     #[test]
     fn err_empty_component_last() {
-        let input = "has.component.";
-        let result = KeyPath::try_from(input).unwrap_err();
+        let input = "empty.component.at.end.";
+        let result = KeyPath::try_from(input).expect_err("invalid keypath");
         assert!(matches!(result, KeyPathParseError::InvalidKeyPath));
     }
 }
